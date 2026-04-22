78737ae9cf50d9848e8141be07183cb49b37f15f
Sanchit Arora <arorasanchit07@gmail.com>
2026-04-22 13:14:17 +0530

    DSA Practice Code

---
+++ Hashing-Linear.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int table[SIZE];
+
+void init() {
+    for (int i = 0; i < SIZE; i++) {
+        table[i] = -1;
+    }
+}
+
+int hashFunction(int key) {
+    return key % SIZE;
+}
+
+void insert(int key) {
+    int index = hashFunction(key);
+    int start = index;
+
+    while (table[index] != -1 && table[index] != -2) {
+        index = (index + 1) % SIZE;
+
+        if (index == start) {
+            printf("Hash Table is Full\n");
+            return;
+        }
+    }
+
+    table[index] = key;
+    printf("Inserted %d at index %d\n", key, index);
+}
+
+void search(int key) {
+    int index = hashFunction(key);
+    int start = index;
+
+    while (table[index] != -1) {
+        if (table[index] == key) {
+            printf("Element found at index %d\n", index);
+            return;
+        }
+
+        index = (index + 1) % SIZE;
+
+        if (index == start)
+            break;
+    }
+
+    printf("Element not found\n");
+}
+
+void deleteKey(int key) {
+    int index = hashFunction(key);
+    int start = index;
+
+    while (table[index] != -1) {
+        if (table[index] == key) {
+            table[index] = -2;   // mark as deleted
+            printf("Element deleted\n");
+            return;
+        }
+
+        index = (index + 1) % SIZE;
+
+        if (index == start)
+            break;
+    }
+
+    printf("Element not found\n");
+}
+
+void display() {
+    printf("\nHash Table:\n");
+    for (int i = 0; i < SIZE; i++) {
+        if (table[i] == -1)
+            printf("%d --> EMPTY\n", i);
+        else if (table[i] == -2)
+            printf("%d --> DELETED\n", i);
+        else
+            printf("%d --> %d\n", i, table[i]);
+    }
+}
+
+int main() {
+    int choice, key;
+
+    init();
+
+    do {
+        printf("\n1. Insert\n2. Search\n3. Delete\n4. Display\n5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter key: ");
+                scanf("%d", &key);
+                insert(key);
+                break;
+
+            case 2:
+                printf("Enter key: ");
+                scanf("%d", &key);
+                search(key);
+                break;
+
+            case 3:
+                printf("Enter key: ");
+                scanf("%d", &key);
+                deleteKey(key);
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice\n");
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
